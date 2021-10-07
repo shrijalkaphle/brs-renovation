302431500354edeb9949bb53ada6ccd72c3e0da1
Shrijal Kaphle <kaphle.shrijal9@gmail.com>
2021-10-07 13:05:27 +0545

    tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      white: '#FFFFFF',
+      primary: {
+        light: '#F6AA26',
+        dark: '#C78513',
+      },
+      secondary: {
+        light: '#091E3A',
+        dark: '#171732',
+      },
+      text: {
+        primary: '#DADBE7'
+      },
+      other: {
+        other1: '#4A5C7A',
+        other2: '#424662',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
